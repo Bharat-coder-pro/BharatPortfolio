18a449b87a5f89f44b64a3e1245d884291867fe8
Bharat Yadav <111577717+Bharat-coder-pro@users.noreply.github.com>
2025-05-28 19:14:53 +0530

    Update NavBar.jsx

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -5,21 +5,29 @@ const NavBar = () => {
     
     }
   return (
-    <div className={`${css.navbar} d-flex flex-wrap px-5 py-1 gap-5 justify-content-start position-fixed`}>
-      <a href="#welcome">
-      <button onClick={(ev)=>navBtn(ev)} id='wcBtn' className={`${css.navItems} btn btn-light fw-bolder text-dark p-2`}>Welcome</button>
-      </a>
-      <a href="#intro"><button onClick={(ev)=>navBtn(ev)} id='introBtn' className={`${css.navItems} btn btn-light fw-bolder text-dark p-2`}>Intro</button></a>
-      <a href="#certs">
-      <button onClick={(ev)=>navBtn(ev)} id='certBtn' className={`${css.navItems} btn btn-light fw-bolder text-dark p-2`}>Certification</button>
-      </a>
-      <a href="#portfolio">
-      <button onClick={(ev)=>navBtn(ev)} id='wsBtn' className={`${css.navItems} btn btn-light fw-bolder text-dark p-2`}>Portfolio</button>
-      </a>
-      <a href="#me">
-      <button onClick={(ev)=>navBtn(ev)} id='contact' className={`${css.navItems} btn btn-light fw-bolder text-dark p-2`}>Contact Me</button> 
-      </a>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#intro"><button onClick={(ev)=>navBtn(ev)} id='introBtn' className={`${css.navItems} btn btn-light fw-bolder text-dark p-2`}>Intro</button></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#certs"><button onClick={(ev)=>navBtn(ev)} id='certBtn' className={`${css.navItems} btn btn-light fw-bolder text-dark p-2`}>Certification</button></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#portfolio"><button onClick={(ev)=>navBtn(ev)} id='wsBtn' className={`${css.navItems} btn btn-light fw-bolder text-dark p-2`}>Portfolio</button></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#me"> <button onClick={(ev)=>navBtn(ev)} id='contact' className={`${css.navItems} btn btn-light fw-bolder text-dark p-2`}>Contact Me</button> </a>
+        </li>
+      </ul>
     </div>
+  </div>
+</nav>
   )
 }
 export default NavBar
